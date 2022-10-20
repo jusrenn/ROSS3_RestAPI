319a0cdee0703f887c52d6b2b43e5a0c797d5446
Yusuf Renkoglu <yusuf@renkoglu.com>
2022-10-21 01:04:06 +0300

    v1

--- src/main/java/com/renkoglu/ross3/RestApiV1/ApiKeyController.java
+++ src/main/java/com/renkoglu/ross3/RestApiV1/ApiKeyController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -22,7 +23,7 @@ public class ApiKeyController {
     }
 
     @GetMapping("/apikeys")
-    public List<ApiKey> apiKeys() {
+    public List<ApiKey> apiKeys(@RequestParam String apiKey) {
         return apiKeyManager.getAll();
     }
 
